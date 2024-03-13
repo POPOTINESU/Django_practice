773b763be666d2f7366d588e2f7a7f50518eae5c
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-03-13 17:48:02 +0900

    add boardModel

--- boards/admin.py
+++ boards/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from .models import message
+from .models import Board
 # Register your models here.
 admin.site.register(message)
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_datetime', 'updated_datetime')
+    list_display_links = ('id','title')
+admin.site.register(Board, BoardAdmin)
--- boards/models.py
+++ boards/models.py
@@ -5,3 +5,11 @@ class message(models.Model):
     body = models.CharField(max_length=200)
     post_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(blank=False, null=False,max_length=150)
+    text = models.TextField(blank=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    updated_datetime = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
